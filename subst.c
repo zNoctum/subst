453a2d48f266770a64121661312f941a6004b4dd
zNoctum <noah.duemling@gmail.com>
2021-06-11 16:08:36 +0200

    changed a thing in the license

--- subst.c
+++ subst.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) zNoctum
+ * Copyright (c) 2021 zNoctum
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal in the Software
